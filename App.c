57d0c5ff2b18d0bad67b3f1c574aa7edd97a2a6a
Alexander <73072695+askelio@users.noreply.github.com>
2021-03-16 15:12:06 +0300

    Add files via upload

---
+++ App.c
@@ -0,0 +1,42 @@
+
+#include "App.h"
+#include <stdio.h>
+#include <conio.h>
+
+/* 
+	Getting the first three numbers after the dot 
+*/
+int Get_Sum(float num)
+{
+	int a;
+	if (num<0)
+	{
+		num=-num;
+	}
+	a = (num - (int)num)*1000;
+	
+	int s = 0;
+	int i; 
+	for( i = 0 ; i < 3; i++)
+	{
+		s += a % 10;
+		a /= 10;
+	}
+	return s;
+
+}
+
+int main()
+{
+	printf("Set the number:\n");
+	float n;
+	scanf("%f",&n);
+	
+	int Sum=Get_Sum(n);
+	
+
+	printf("The sum of the first three numbers after the dot: ");
+	printf("%d",Sum);
+    getch();
+	return 0;
+}
